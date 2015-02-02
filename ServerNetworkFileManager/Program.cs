3917b90d977fb7b24a23c7d136c9b69b4e6a0b9a
Руслан <ruslan910425@gmail.com>
2015-02-02 21:31:46 +0600

    исправлены ошибки передачи файлов теперь все работает - было протестировано

--- ServerNetworkFileManager/Program.cs
+++ ServerNetworkFileManager/Program.cs
@@ -17,30 +17,46 @@ namespace ServerNetworkFileManager
         // Прием сетевых файлов
         //********************************
 
-        public StreamWriter sw;
-        public NetworkReciever()
-        {
-            //sw = new StreamWriter("/ais/logs/logReciever.txt");
-            sw = new StreamWriter("/logReciever.txt");
-        }
+        private string _pathToLogFile = null;
+        private string port = "3333";
+        private string serverIP = null;
+
+        private int countOfBytesInBuffer = 1024;
+        private int countOfClient = 100;
+        private string pathToSaveFolder = "/";
 
         private Stream strLocal = null;
         private NetworkStream strRemote = null;
         private TcpListener tlsServer = null;
-        private String port = "3333",
-                         serverIP = "172.16.16.8";
-                     //  serverIP = "172.16.1.24";
-        private int countOfClient = 100;
         private bool isError = false;
-        //private string pathToParDir = "/ais/data/";
-        private string pathToParDir = "/";
+        private List<string> logMas = new List<string>();
 
-        public void SetServerIP(String ip)
+        public NetworkReciever(string pathToLogFile)
+        {
+            _pathToLogFile = pathToLogFile;
+
+            serverIP = GetCurrentIP();
+        }
+
+        public NetworkReciever()
+        {
+            serverIP = GetCurrentIP();
+        }
+
+        public string GetCurrentIP()
+        {
+            String host = System.Net.Dns.GetHostName();
+            System.Net.IPAddress ip = System.Net.Dns.GetHostByName(host).AddressList[0];
+
+            return ip.ToString();
+        }
+
+        public void SetServerIP(string ip)
         {
             serverIP = ip;
         }
 
-        public void SetPort(String port)
+        public void SetPort(string port)
         {
             this.port = port;
         }
@@ -56,28 +72,29 @@ namespace ServerNetworkFileManager
                 }
                 UpdateStatus("Starting the server...");
                 tlsServer.Start(countOfClient);
-                UpdateStatus("The server has started. Please connect the client to " + ipLocal.ToString() + ".");
+                UpdateStatus("The server has started");
+                UpdateStatus("Please connect the client to " + ipLocal.ToString());
                 TcpClient client = tlsServer.AcceptTcpClient();
-                UpdateStatus("The server has accepted the client.");
+                UpdateStatus("The server has accepted the client");
                 strRemote = client.GetStream();
-                UpdateStatus("The server has received the stream.");
+                UpdateStatus("The server has received the stream");
 
                 int bytesSize = 0;
-                byte[] downBuffer = new byte[2048];
-                bytesSize = strRemote.Read(downBuffer, 0, 2048);
+                byte[] downBuffer = new byte[countOfBytesInBuffer];
+                bytesSize = strRemote.Read(downBuffer, 0, countOfBytesInBuffer);
 
                 string strFileName = System.Text.Encoding.UTF8.GetString(downBuffer, 0, bytesSize);
                 strFileName = strFileName.Substring(0, strFileName.IndexOf('\n'));
-                strLocal = new FileStream(pathToParDir + strFileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+                strLocal = new FileStream(pathToSaveFolder + strFileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
 
-                downBuffer = new byte[2048];
-                bytesSize = strRemote.Read(downBuffer, 0, 2048);
+                downBuffer = new byte[countOfBytesInBuffer];
+                bytesSize = strRemote.Read(downBuffer, 0, countOfBytesInBuffer);
                 string strFileSize = System.Text.Encoding.UTF8.GetString(downBuffer, 0, bytesSize);
                 strFileSize = strFileSize.Substring(0, strFileSize.IndexOf('\n'));
                 long FileSize = Convert.ToInt64(strFileSize);
 
                 UpdateStatus("Receiving file " + strFileName + " (" + FileSize + " bytes)");
-                downBuffer = new byte[2048];
+                downBuffer = new byte[countOfBytesInBuffer];
 
                 while ((bytesSize = strRemote.Read(downBuffer, 0, downBuffer.Length)) > 0)
                 {
@@ -94,10 +111,10 @@ namespace ServerNetworkFileManager
             {
                 if (!isError)
                 {
-                    UpdateStatus("The file was received.");
+                    UpdateStatus("The file was received");
                 }
                 else
-                    UpdateStatus("The file was NOT received.");
+                    UpdateStatus("The file was NOT received");
                 isError = false;
 
                 if (strLocal != null)
@@ -109,22 +126,29 @@ namespace ServerNetworkFileManager
                 }
                 if (strRemote != null)
                 {
+                    strRemote.Flush();
                     strRemote.Close();
                     strRemote.Dispose();
                     strRemote = null;
                 }
-                UpdateStatus("Streams are now closed.");
+                UpdateStatus("Streams are now closed \n");
 
-                sw.Flush();
-                GC.Collect();
+                // записываем лог и чистим промежуточный лист
+                if (_pathToLogFile != null)
+                {
+                    File.AppendAllLines(_pathToLogFile, logMas);
+                    logMas.Clear();
+                }
+
+                // заново запускаем прослушивание...
                 StartReceiving();
             }
         }
 
-        private void UpdateStatus(string StatusMessage)
+        private void UpdateStatus(string statusMessage)
         {
-            Console.WriteLine(StatusMessage);
-            sw.WriteLine(StatusMessage);
+            Console.WriteLine(statusMessage);
+            logMas.Add(statusMessage);
         }
     }
 
@@ -134,19 +158,25 @@ namespace ServerNetworkFileManager
         // Отправка сетевых файлов
         //********************************
 
+        private int countOfBytesInBuffer = 1024;
+        private string clientIP = "172.16.1.24";
+        private int clientPort = 3333;
+
         private TcpClient tcpClient = null;
         private FileStream fstFile = null;
         private bool isError = false;
         private NetworkStream strRemote = null;
-        public StreamWriter sw = null;
-        private string pathToLogFile = "/ais/logs/logSender.txt";
+        private string _pathToLogFile = null;
+        List<string> logMas = new List<string>();
 
-        private string clientIP = "172.16.16.8";
-        private int clientPort = 3333;
+        public NetworkSender(string pathToLogFile)
+        {
+            _pathToLogFile = pathToLogFile;
+        }
 
         public NetworkSender()
         {
-            sw = new StreamWriter(pathToLogFile);
+
         }
 
         private void ConnectToServer()
@@ -155,7 +185,7 @@ namespace ServerNetworkFileManager
             try
             {
                 tcpClient.Connect(clientIP, clientPort);
-                UpdateStatus("Successfully connected to server.");
+                UpdateStatus("Successfully connected to server");
             }
             catch (Exception exMessage)
             {
@@ -163,7 +193,7 @@ namespace ServerNetworkFileManager
             }
         }
 
-        public void SendFile(String pathToFile)
+        public void SendFile(string pathToFile)
         {
             try
             {
@@ -171,26 +201,25 @@ namespace ServerNetworkFileManager
                 {
                     ConnectToServer();
                 }
-                UpdateStatus("Sending file information.");
+                UpdateStatus("Sending file information");
                 strRemote = tcpClient.GetStream();
                 byte[] byteSend = new byte[tcpClient.ReceiveBufferSize];
                 fstFile = new FileStream(pathToFile, FileMode.Open, FileAccess.Read);
                 BinaryReader binFile = new BinaryReader(fstFile);
 
                 FileInfo fInfo = new FileInfo(pathToFile);
-                string FileName = fInfo.Name;
-                byte[] ByteFileName = new byte[1024];
-                FileName = FileName + "\n";
-                byte[] tempByteFileName = System.Text.Encoding.UTF8.GetBytes(FileName.ToCharArray());
+                string fileName = fInfo.Name;
+                byte[] ByteFileName = new byte[countOfBytesInBuffer];
+                byte[] tempByteFileName = System.Text.Encoding.UTF8.GetBytes( (fileName + "\n").ToCharArray() );
 
                 for (int i = 0; i < tempByteFileName.Length; i++)
                 {
                     ByteFileName[i] = tempByteFileName[i];
                 }
-                strRemote.Write(ByteFileName, 0, 1024);
+                strRemote.Write(ByteFileName, 0, countOfBytesInBuffer);
 
                 long FileSize = fInfo.Length;
-                byte[] ByteFileSize = new byte[1024];
+                byte[] ByteFileSize = new byte[countOfBytesInBuffer];
                 string strFileSize = FileSize.ToString();
                 strFileSize = strFileSize + "\n";
                 byte[] tempByteFileSize = System.Text.Encoding.UTF8.GetBytes(strFileSize.ToCharArray());
@@ -199,12 +228,12 @@ namespace ServerNetworkFileManager
                 {
                     ByteFileSize[i] = tempByteFileSize[i];
                 }
-                strRemote.Write(ByteFileSize, 0, 2048);
+                strRemote.Write(ByteFileSize, 0, countOfBytesInBuffer);
 
-                UpdateStatus("Sending the file " + FileName + ".");
+                UpdateStatus("Sending the file '" + fileName + "'");
 
                 int bytesSize = 0;
-                byte[] downBuffer = new byte[2048];
+                byte[] downBuffer = new byte[countOfBytesInBuffer];
 
                 while ((bytesSize = fstFile.Read(downBuffer, 0, downBuffer.Length)) > 0)
                 {
@@ -221,11 +250,11 @@ namespace ServerNetworkFileManager
             {
                 if (!isError)
                 {
-                    UpdateStatus("File sent.");
+                    UpdateStatus("File sent");
                 }
                 else
                 {
-                    UpdateStatus("File does NOT sent.");
+                    UpdateStatus("File does NOT sent");
                 }
                 isError = false;
 
@@ -236,179 +265,33 @@ namespace ServerNetworkFileManager
                 }
                 if (strRemote != null)
                 {
+                    strRemote.Flush();
                     strRemote.Close();
                     strRemote.Dispose();
                     strRemote = null;
                 }
                 if (fstFile != null)
                 {
+                    fstFile.Flush();
                     fstFile.Close();
                     fstFile.Dispose();
                     fstFile = null;
                 }
-                UpdateStatus("Streams and connections are now closed.");
-                sw.Flush();
-                GC.Collect();
-            }
-        }
+                UpdateStatus("Streams and connections are now closed \n");
 
-        private void UpdateStatus(string StatusMessage)
-        {
-            Console.WriteLine(StatusMessage);
-            sw.WriteLine(StatusMessage);
-        }
-    }
-
-    class AsyncNetworkReciever
-    {
-        //********************************
-        // Прием сетевых файлов
-        //********************************
-
-        private string pathToLogFile = "/logReciever.txt",
-                       pathToDownloadDir = "/",
-                       port = "3333",
-                       serverIP = "";
-        public AsyncNetworkReciever()
-        {
-            sw = new StreamWriter(pathToLogFile);
-         
-            if (serverIP == "")
-            {
-                // Получение имени компьютера.
-                String host = System.Net.Dns.GetHostName();
-                // Получение ip-адреса.
-                System.Net.IPAddress ip = Dns.GetHostByName(host).AddressList[0];
-                serverIP = ip.ToString();
-            }
-        }
-
-        public StreamWriter sw = null;
-        private Stream strLocal = null;
-        private NetworkStream strRemote = null;
-        private TcpListener listener = null;
-        private bool isError = false;
-        
-        public static ManualResetEvent tcpClientConnected = new ManualResetEvent(false);
-
-        public void SetServerIP(String ip)
-        {
-            serverIP = ip;
-        }
-
-        public void SetPort(String port)
-        {
-            this.port = port;
-        }
-
-        // Accept one client connection asynchronously.
-        public void StartAsyncReceiving()
-        {
-            // Set the event to nonsignaled state.
-            tcpClientConnected.Reset();
-
-            // Start to listen for connections from a client.
-            UpdateStatus("Waiting for a connection...");
-
-            // Accept the connection. 
-            // BeginAcceptSocket() creates the accepted socket.
-            listener.BeginAcceptTcpClient(new AsyncCallback(DoAcceptTcpClientCallback), listener);
-
-            // Wait until a connection is made and processed before 
-            // continuing.
-
-            tcpClientConnected.WaitOne();
-        }
-
-        // Process the client connection.
-        public void DoAcceptTcpClientCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Get the listener that handles the client request.
-                TcpListener listener = (TcpListener)ar.AsyncState;
-
-                // End the operation and display the received data on the console.
-                TcpClient client = listener.EndAcceptTcpClient(ar);
-                UpdateStatus("The server has accepted the client.");
-                strRemote = client.GetStream();
-                UpdateStatus("The server has received the stream.");
-
-                int bytesSize = 0;
-                byte[] downBuffer = new byte[2048];
-                bytesSize = strRemote.Read(downBuffer, 0, 2048);
-
-                string strFileName = System.Text.Encoding.UTF8.GetString(downBuffer, 0, bytesSize);
-                strFileName = strFileName.Substring(0, strFileName.IndexOf('\n'));
-                strLocal = new FileStream(pathToDownloadDir + strFileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
-
-                downBuffer = new byte[2048];
-                bytesSize = strRemote.Read(downBuffer, 0, 2048);
-                string strFileSize = System.Text.Encoding.UTF8.GetString(downBuffer, 0, bytesSize);
-                strFileSize = strFileSize.Substring(0, strFileSize.IndexOf('\n'));
-                long FileSize = Convert.ToInt64(strFileSize);
-
-                UpdateStatus("Receiving file " + strFileName + " (" + FileSize + " bytes)");
-                downBuffer = new byte[2048];
-
-                while ((bytesSize = strRemote.Read(downBuffer, 0, downBuffer.Length)) > 0)
+                // записываем лог
+                if (_pathToLogFile != null)
                 {
-                    strLocal.Write(downBuffer, 0, bytesSize);
+                    File.AppendAllLines(_pathToLogFile, logMas);
+                    logMas.Clear();
                 }
             }
-            catch (Exception ex)
-            {
-                UpdateStatus("Erorr!!!");
-                UpdateStatus(ex.Message);
-                isError = true;
-            }
-            finally
-            {
-                if (!isError)
-                {
-                    UpdateStatus("The file was received.");
-                }
-                else
-                    UpdateStatus("The file was NOT received.");
-                isError = false;
-                if (strLocal != null)
-                {
-                    strLocal.Flush();
-                    strLocal.Close();
-                    strLocal.Dispose();
-                    strLocal = null;
-                }
-                if (strRemote != null)
-                {
-                    strRemote.Close();
-                    strRemote.Dispose();
-                    strRemote = null;
-                }
-                UpdateStatus("Streams are now closed.");
-                sw.Flush();
-                GC.Collect();
-                // Signal the calling thread to continue.
-                tcpClientConnected.Set();
-
-                StartAsyncReceiving();
-            }
         }
 
-        public void initListener()
+        private void UpdateStatus(string statusMessage)
         {
-            if (listener == null)
-            {
-                listener = new TcpListener(IPAddress.Parse(serverIP), Int32.Parse(port));
-            }
-            UpdateStatus("Starting the server...");
-            listener.Start();
-            UpdateStatus("The server has started. Please connect the client to " + serverIP + ".");
-        }
-
-        private void UpdateStatus(string StatusMessage)
-        {
-            Console.WriteLine(StatusMessage);
-            sw.WriteLine(StatusMessage);
+            Console.WriteLine(statusMessage);
+            logMas.Add(statusMessage);
         }
     }
 
@@ -416,12 +299,15 @@ namespace ServerNetworkFileManager
     {
         static void Main(string[] args)
         {
-            // NetworkSender ns = new NetworkSender();
-            // ns.SendFile("/ais/Procc.bmp");
+            //NetworkSender ns = new NetworkSender();
+            //ns.SendFile(args[0]);
 
-            NetworkReciever nr = new NetworkReciever();
-            nr.StartReceiving();
-            
+            NetworkSender ns = new NetworkSender();
+            ns.SendFile("/генетический алгоритм.pdf");
+
+            //NetworkReciever nr = new NetworkReciever();
+            //nr.StartReceiving();
+
             //AsyncNetworkReciever anr = new AsyncNetworkReciever();
             //anr.initListener();
             //anr.StartAsyncReceiving();
